9a411cdf311706cfb8ffd478d6772ca7d1cfed7e
KyamranR <kyamranr@gmail.com>
2024-01-25 22:33:36 -0600

    add start, questions, answer and thankyou routes

--- app.py
+++ app.py
@@ -1,18 +1,53 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
-import sys
+from surveys import satisfaction_survey
 
-print(sys.path)
 
 app = Flask(__name__)
 
-app.debug = True
+app.debug = False
 
 app.config['SECRET_KEY'] = 'porsche'
 
 toolbar = DebugToolbarExtension(app)
 
+responses = []
 
 @app.route('/')
-def home():
-    return 'Home Page'
+def start_survey():
+    """Starting the survey"""
+    return render_template('start.html', survey=satisfaction_survey)
+
+
+@app.route('/questions/<int:question_id>')
+def show_question(question_id):
+    """Check if the question_id is valid, if not redirect to the correct URL"""
+    if question_id < len(responses):
+        return redirect(f"/questions/{len(responses)}")
+    
+    if question_id == len(responses):
+        if question_id < len(satisfaction_survey.questions):
+            current_question = satisfaction_survey.questions[question_id]
+            return render_template('question.html', question=current_question, question_id=question_id)
+        else:
+            return redirect("/thankyou")
+    flash("Invalid question access. Please answer questions in order")
+    return redirect(f"/questions/{len(responses)}")
+
+@app.route('/answer', methods=["POST"])
+def handle_answer():
+    """Handling answer submission"""
+    answer = request.form.get("answer")
+    responses.append(answer)
+
+    if len(responses) == len(satisfaction_survey.questions):
+        return redirect("/thankyou")
+    
+    else:
+        return redirect(f"/questions/{len(responses)}")
+    
+
+@app.route('/thankyou')
+def thank_you():
+    """Basic Thank you"""
+    return "Thank you"
